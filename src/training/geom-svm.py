b10900c01c6e43011aaaf41fe072dcbb5dc07f61
Marilyn Stanwood <nyliz@live.com>
2026-04-02 13:36:09 +0200

    newexist_option A noscaler

--- src/training/geom-svm.py
+++ src/training/geom-svm.py
@@ -57,7 +57,7 @@ if "df" not in globals():
 # -----------------------------------------------------------------------------
 print("[SECTION] Configuring preprocessing options")
 noemp_option: str = "C"
-newexist_option: str = "C"
+newexist_option: str = "A" # do not change
 createjob_option: str = "B" # same
 retainedjob_option: str = "B" # same
 disbursementgross_option: str = "B" 
